5a2e54f50a6658a22954859a03ed93c3c17d3f46
Atle Frenvik Sveen <atle.sveen@gmail.com>
2020-12-06 14:21:00 +0100

    day, functional all the way

--- day6/index.js
+++ day6/index.js
@@ -1,25 +1,34 @@
 const readFile = require("../readFile");
 
+const merge = (l1, l2) => [...l1, ...l2];
+
+const sum = (lst) => lst.reduce((s, e) => s + e.length, 0);
+
+const distinct = (lst) => Array.from(new Set(lst));
+
+const flatten = (lst) => lst.reduce((acc, lst) => [...lst, ...acc], []);
+
 const parseGroup = (group) => group.split("\n").map((p) => p.split(""));
 
 const read = (file) => readFile(file, parseGroup, "\n\n");
 
-const distinctInList = (lst) => Array.from(new Set(lst));
+const includes = (element) => (subLst) => subLst.includes(element);
 
-const getDistinct = (lst) =>
-  distinctInList(lst.reduce((acc, lst) => [...lst, ...acc], []));
+const allListsIncludes = (lstOfLsts) => (e) => lstOfLsts.every(includes(e));
 
-const getCommon = (l, a) => l.filter((x) => a.every((ee) => ee.includes(x)));
+const overlap = (list, lstOfLsts) => list.filter(allListsIncludes(lstOfLsts));
 
-const getInAll = (lst) =>
-  distinctInList(lst.reduce((acc, e) => [...getCommon(e, lst), ...acc], []));
+const common = (lst) => lst.reduce((acc, e) => merge(overlap(e, lst), acc), []);
 
-const count = (lst) => lst.reduce((s, e) => s + e.length, 0);
+const isInAll = (lst) => distinct(common(lst));
 
-const task1 = async (filename) =>
-  count((await read(filename)).map(getDistinct));
+const isInAny = (lst) => distinct(flatten(lst));
 
-const task2 = async (filename) => count((await read(filename)).map(getInAll));
+const genericTask = async (filename, f) => sum((await read(filename)).map(f));
+
+const task1 = async (filename) => genericTask(filename, isInAny);
+
+const task2 = async (filename) => genericTask(filename, isInAll);
 
 const main = async () => {
   const test1 = await task1("testinput.txt");
