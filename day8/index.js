eaac15002c3b4908b7890f1a3c62b739135963b3
Atle Frenvik Sveen <atle.sveen@gmail.com>
2020-12-08 10:03:24 +0100

    rewrite day8

--- day8/index.js
+++ day8/index.js
@@ -1,63 +1,52 @@
 const readFile = require("../readFile");
 
-const parseLine = (line) => {
-  const [instruction, value] = line.split(" ");
-  return { instruction, value: parseInt(value, 10) };
-};
+const formatLine = (instruction, value) => ({
+  instruction,
+  value: parseInt(value, 10),
+});
 
-const read = (filename) => readFile(filename, parseLine);
+const parseLine = (line) => formatLine(...line.split(" "));
 
-const flip = (i) => (i === "jmp" ? "nop" : "jmp");
+const readProgram = (filename) => readFile(filename, parseLine);
+
+const flip = (op) => ({ jmp: "nop", nop: "jmp" }[op]);
 
 const replace = (program, idx) =>
   program.map((p, i) =>
-    i === idx ? { instruction: flip(p.instruction), value: p.value } : p
+    i === idx ? { ...p, instruction: flip(p.instruction) } : p
   );
 
-const execute = (program) => {
-  let acc = 0;
-  let idx = 0;
+const match = (d, val) => d[val]();
 
-  const used = [];
+const eval = (op, idx, acc) =>
+  match(
+    {
+      acc: () => [idx + 1, acc + op.value],
+      nop: () => [idx + 1, acc],
+      jmp: () => [idx + op.value, acc],
+    },
+    op.instruction
+  );
 
-  while (true) {
-    const op = program[idx];
-
-    if (used.includes(idx)) {
-      return { completed: false, acc };
-    }
-    if (!op) {
-      return { completed: true, acc };
-    }
-    used.push(idx);
-
-    if (op.instruction === "acc") {
-      acc += op.value;
-      idx += 1;
-    }
-    if (op.instruction === "nop") {
-      idx += 1;
-    }
-    if (op.instruction === "jmp") {
-      idx += op.value;
-    }
-  }
-};
+const execute = (program, idx = 0, acc = 0, used = []) =>
+  used.includes(idx)
+    ? { exitCode: 1, acc }
+    : idx < 0 || idx >= program.length
+    ? { exitCode: 0, acc }
+    : execute(program, ...eval(program[idx], idx, acc), [idx, ...used]);
 
 const task1 = (program) => execute(program).acc;
 
 const task2 = (program) =>
   program
-    .map((op, i) =>
-      op.instruction === "jmp" || op.instruction === "nop" ? i : null
-    )
+    .map((op, i) => (["jmp", "nop"].includes(op.instruction) ? i : null))
     .filter((x) => x !== null)
-    .map((j) => execute(replace(program, j)))
-    .find((r) => r.completed).acc;
+    .map((idx) => execute(replace(program, idx)))
+    .find((res) => res.exitCode === 0).acc;
 
 const main = async () => {
-  const testinput = await read("testinput.txt");
-  const input = await read("input.txt");
+  const testinput = await readProgram("testinput.txt");
+  const input = await readProgram("input.txt");
 
   console.log(task1(testinput) === 5);
   console.log(task1(input) === 1600);
