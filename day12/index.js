328883eaeaa816db1fa2bf9d04e44848b7b55926
Atle Frenvik Sveen <atle.sveen@gmail.com>
2020-12-22 13:46:08 +0100

    clanuo day 12

--- day12/index.js
+++ day12/index.js
@@ -2,10 +2,17 @@ const readFile = require("../readFile");
 
 const DIRECTIONS = ["N", "E", "S", "W"];
 
-const parseIns = (ins) => {
-  const [_, direction, units] = /([A-Z])(\d*)/.exec(ins);
-  return { direction, units: parseInt(units, 10) };
-};
+const getRegexGroups = (regex, groups) => (str) =>
+  regex.exec(str).filter((_, i) => groups.includes(i));
+
+const parseIns = (ins) =>
+  getRegexGroups(/([A-Z])(\d*)/, [1, 2])(ins).reduce(
+    (acc, e, i) => ({
+      ...acc,
+      [i === 0 ? "direction" : "units"]: i === 0 ? e : parseInt(e, 10),
+    }),
+    {}
+  );
 
 const getSign = (heading) => (heading === "N" || heading === "E" ? 1 : -1);
 
@@ -29,23 +36,22 @@ const updateHeading = (heading, dir) =>
 const getHeading = (heading, direction) =>
   DIRECTIONS.includes(direction) ? direction : heading;
 
-const run = (directions, heading, pos) => {
-  const dir = directions[0];
-  if (!dir) {
-    return pos;
-  }
-
-  if (["L", "R"].includes(dir.direction)) {
-    return run(directions.slice(1), updateHeading(heading, dir), pos);
-  }
-  if (["F", ...DIRECTIONS].includes(dir.direction)) {
-    return run(
-      directions.slice(1),
-      heading,
-      updatePos(pos, getHeading(heading, dir.direction), dir.units)
-    );
-  }
-};
+const run = (directions, heading, pos) =>
+  directions.length === 0
+    ? pos
+    : ["L", "R"].includes(directions[0].direction)
+    ? run(directions.slice(1), updateHeading(heading, directions[0]), pos)
+    : ["F", ...DIRECTIONS].includes(directions[0].direction)
+    ? run(
+        directions.slice(1),
+        heading,
+        updatePos(
+          pos,
+          getHeading(heading, directions[0].direction),
+          directions[0].units
+        )
+      )
+    : null;
 
 const manhattanDist = (pos) => Math.abs(pos[0]) + Math.abs(pos[1]);
 
@@ -63,36 +69,31 @@ const rotate = (p, a) => [
   Math.round(Math.sin(toRad(a)) * p[0] + Math.cos(toRad(a)) * p[1]),
 ];
 
-const moveForward = (directions, dir, pos, wp) =>
-  run2(directions.slice(1), moveToWaypoint(pos, wp, dir.units), wp);
+const moveForward = (dir, pos, wp) => [moveToWaypoint(pos, wp, dir.units), wp];
 
-const moveWaypoint = (directions, dir, pos, wp) =>
-  run2(directions.slice(1), pos, updatePos(wp, dir.direction, dir.units));
+const moveWaypoint = (dir, pos, wp) => [
+  pos,
+  updatePos(wp, dir.direction, dir.units),
+];
 
-const rotateWaypoint = (directions, dir, pos, wp) =>
-  run2(
-    directions.slice(1),
-    pos,
-    rotate(wp, dir.units * (dir.direction === "R" ? 1 : -1))
-  );
+const rotateWaypoint = (dir, pos, wp) => [
+  pos,
+  rotate(wp, dir.units * (dir.direction === "R" ? 1 : -1)),
+];
 
-const run2 = (directions, pos, wp) => {
-  const dir = directions[0];
-
-  if (!dir) {
-    return pos;
-  }
-
-  if (dir.direction === "F") {
-    return moveForward(directions, dir, pos, wp);
-  }
-  if (DIRECTIONS.includes(dir.direction)) {
-    return moveWaypoint(directions, dir, pos, wp);
-  }
-  if (["L", "R"].includes(dir.direction)) {
-    return rotateWaypoint(directions, dir, pos, wp);
-  }
-};
+const run2 = (directions, pos, wp) =>
+  directions.length === 0
+    ? pos
+    : run2(
+        directions.slice(1),
+        ...(directions[0].direction === "F"
+          ? moveForward(directions[0], pos, wp)
+          : DIRECTIONS.includes(directions[0].direction)
+          ? moveWaypoint(directions[0], pos, wp)
+          : ["L", "R"].includes(directions[0].direction)
+          ? rotateWaypoint(directions[0], pos, wp)
+          : [])
+      );
 
 const task2 = (input) =>
   manhattanDist(run2(input.map(parseIns), [0, 0], [1, 10]));
